3991dd6defb4ba4c9b90ca90c62c07918043ddc9
Felipe Sobreira <felipesobreira.c@hotmail.com>
2020-09-07 20:13:07 -0300

    formataçao de moeda e percentual dos valores

--- src/components/InputReadOnly.js
+++ src/components/InputReadOnly.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { formatMoney, formatPercentage } from '../helpers/formatters';
 
 export default class InputReadOnly extends Component {
   constructor() {
@@ -8,8 +9,10 @@ export default class InputReadOnly extends Component {
   render() {
     const { color = 'black', value, percentage = 0, label } = this.props;
 
-    const formattedPercentage = percentage > 0 ? `(${percentage})` : '';
-    const formattedValue = `${value} ${formattedPercentage}`;
+    const formattedPercentage =
+      percentage > 0 ? `(${formatPercentage(percentage)})` : '';
+
+    const formattedValue = `${formatMoney(value)} ${formattedPercentage}`;
 
     return (
       <div className="input-field col s12 m6 l3">
